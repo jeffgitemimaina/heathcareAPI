2f2e2c3c950976b3c4450a3010821001f24ee1a4
jeffgitemimaina <gitemijeff@gmail..com>
2025-04-27 02:27:01 +0300

    Fix EnableJpaRepositories import in HealthProgramApplication

--- src/main/java/com/healthcare_api/healthcare_api/HealthProgramApplication.java
+++ src/main/java/com/healthcare_api/healthcare_api/HealthProgramApplication.java
@@ -2,10 +2,12 @@ package com.healthcare_api.healthcare_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.healthcare_api.healthcare_api.repository")
 public class HealthProgramApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
